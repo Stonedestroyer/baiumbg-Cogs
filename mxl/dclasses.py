d09b3537afd1d1fbcf1fde3621d9df1237951a3a
Veselin Dobrev <veselin.dobrev@jboxers.com>
2019-03-05 18:07:33 +0200

    WIP

--- mxl/dclasses.py
+++ mxl/dclasses.py
@@ -99,9 +99,9 @@ class ItemDump:
     def to_trade_post(self):
         items_section = ''
         sets_str = ''
-        for set_ in sorted(self.sets.values(), key=lambda kv: kv[1].name):
+        for set_ in sorted(self.sets.values(), key=lambda k: k.name):
             sets_str += f'[u][color=#00FF00]{set_.name}[/color][/u]\n'
-            for item in sorted(set_.items.values(), key=lambda kv: kv[1].name):
+            for item in sorted(set_.items.values(), key=lambda k: k.name):
                 sets_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
             sets_str += '\n'
@@ -110,96 +110,96 @@ class ItemDump:
             items_section += TRADE_POST_SETS_SECTION.format(items = sets_str)
 
         su_str = ''
-        for item in sorted(self.su.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.su.values(), key=lambda k: k.name):
             su_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if su_str:
             items_section += TRADE_POST_SU_SECTION.format(items = su_str)
 
         ssu_str = ''
-        for item in sorted(self.ssu.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.ssu.values(), key=lambda k: k.name):
             ssu_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if ssu_str:
             items_section += TRADE_POST_SSU_SECTION.format(items = ssu_str)
 
         sssu_str = ''
-        for item in sorted(self.sssu.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.sssu.values(), key=lambda k: k.name):
             sssu_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if sssu_str:
             items_section += TRADE_POST_SSSU_SECTION.format(items = sssu_str)
 
         runewords_str = ''
-        for item in sorted(self.runewords.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.runewords.values(), key=lambda k: k.name):
             runewords_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if runewords_str:
             items_section += TRADE_POST_RUNEWORDS_SECTION.format(items = runewords_str)
 
         raqmoj_str = ''
-        for item in sorted(self.rings.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.rings.values(), key=lambda k: k.name):
             raqmoj_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if self.rings:
             raqmoj_str += '\n'
 
-        for item in sorted(self.amulets.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.amulets.values(), key=lambda k: k.name):
             raqmoj_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if self.amulets:
             raqmoj_str += '\n'
 
-        for item in sorted(self.quivers.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.quivers.values(), key=lambda k: k.name):
             raqmoj_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if self.quivers:
             raqmoj_str += '\n'
 
-        for item in sorted(self.mos.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.mos.values(), key=lambda k: k.name):
             raqmoj_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if self.mos:
             raqmoj_str += '\n'
 
-        for item in sorted(self.jewels.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.jewels.values(), key=lambda k: k.name):
             raqmoj_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if raqmoj_str:
             items_section += TRADE_POST_RAQMOJ_SECTION.format(items = raqmoj_str)
 
         bases_str = ''
-        for item in sorted(self.rw_bases.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.rw_bases.values(), key=lambda k: k.name):
             bases_str += f'[color=#808080]{item.name}[/color] x{item.amount}\n' if item.amount > 1 else f'[color=#808080]{item.name}[/color]\n'
 
-        for item in sorted(self.shrine_bases.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.shrine_bases.values(), key=lambda k: k.name):
             bases_str += f'[color=#FFFF00]{item.name}[/color] x{item.amount}\n' if item.amount > 1 else f'[color=#FFFF00]{item.name}[/color]\n'
 
         if bases_str:
             items_section += TRADE_POST_BASES_SECTION.format(items = bases_str)
 
         charms_str = ''
-        for item in sorted(self.charms.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.charms.values(), key=lambda k: k.name):
             charms_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if charms_str:
             items_section += TRADE_POST_CHARMS_SECTION.format(items = charms_str)
 
         trophies_str = ''
-        for item in sorted(self.trophies.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.trophies.values(), key=lambda k: k.name):
             trophies_str += f'[color=#FF7F50]{item.name}[/color] x{item.amount}\n' if item.amount > 1 else f'[color=#FF7F50]{item.name}[/color]\n'
 
         if trophies_str:
             items_section += TRADE_POST_TROPHIES_SECTION.format(items = trophies_str)
 
         other_str = ''
-        for item in sorted(self.shrines.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.shrines.values(), key=lambda k: k.name):
             other_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if self.shrines:
             other_str += '\n'
 
-        for item in sorted(self.other.values(), key=lambda kv: kv[1].name):
+        for item in sorted(self.other.values(), key=lambda k: k.name):
             other_str += f'[item]{item.name}[/item] x{item.amount}\n' if item.amount > 1 else f'[item]{item.name}[/item]\n'
 
         if other_str:
